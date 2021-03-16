27fde3e2f568a09c0c416b3bed32bcd56a0a3262
williams <williamskamga0@gmail.com>
2021-03-16 01:53:00 -0400

    Login Signin page

---
+++ src/Components/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../Css/App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./Login";
+import SignUp from "./SignUp";
+
+function App() {
+  return (
+  <Router>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"} style={{fontFamily:"Didot"}}>Financial Freedom</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Sign in</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="outer">
+        <div className="inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
+  );
+}
+
+export default App;
---
+++ src/Components/Login.js
@@ -0,0 +1,50 @@
+import React, {useState} from "react";
+
+export default function Login () {
+    const [UserInfo, setUserInfo] = useState({
+        Email: "",
+        Password: ""
+    });
+    const handleChange = (e) => {
+        const target = e.target;
+        const value = target.value
+        const name = target.name
+
+        setUserInfo({
+            [name]:value
+        })
+    }
+    const handleClick = ()=> {
+        console.log("User Info:");
+        console.log(UserInfo.Email)
+    }
+
+    return (
+        <form>
+
+            <h3>Log in</h3>
+
+            <div className="form-group">
+                <label htmlFor="Email">Email</label>
+                <input id="Email" type="email" className="form-control" placeholder="Enter email" value={UserInfo.Email} onChange={handleChange}/>
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="Password">Password</label>
+                <input id="Password" type="password" className="form-control" placeholder="Enter password" value={UserInfo.Password} onChange={handleChange}/>
+            </div>
+
+            <div className="form-group">
+                <div className="custom-control custom-checkbox">
+                    <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                    <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                </div>
+            </div>
+
+            <button type="submit" className="btn btn-dark btn-lg btn-block" onClick={handleClick}>Sign in</button>
+            <p className="forgot-password text-right">
+                Forgot <a href="#">password?</a>
+            </p>
+        </form>
+    );
+}
